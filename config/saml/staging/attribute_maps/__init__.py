321eda64a52aaa3ccc893a10f9a38a865f567d10
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2017-11-03 14:46:38 +0000

    Add UKEF mappings for staging and prod

--- config/saml/staging/attribute_maps/__init__.py
+++ config/saml/staging/attribute_maps/__init__.py
@@ -1,12 +1,6 @@
 # this is created specifically to match Core's ADFS IdP response
 MAP = {
     'identifier': 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
-    # 'fro': {
-    #     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress': 'email',
-    #     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname': 'first_name',
-    #     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname': 'last_name',
-    #     'http://schemas.xmlsoap.org/claims/Group': 'group',
-    # },
     'fro': {
         'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email': 'email',
         'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/first_name': 'first_name',
@@ -16,7 +10,6 @@ MAP = {
         'last_name': 'last_name',
         'email': 'email'
     },
-
     'to': {
         'email': 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress',
         'first_name': 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname',
