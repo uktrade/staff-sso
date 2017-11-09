978f8cd750defa383ca92002911a110957d75192
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2017-11-09 17:09:02 +0000

    Allow user's to be created and editted via admin

--- sso/user/admin.py
+++ sso/user/admin.py
@@ -6,12 +6,10 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     search_fields = ('email', )
-    fields = ('email', 'is_superuser', 'date_joined', 'last_login', 'permitted_applications')
-    readonly_fields = ('email', 'password', 'date_joined', 'last_login')
+    fields = ('email', 'first_name', 'last_name',  'is_superuser', 'date_joined', 'last_login', 'permitted_applications')
+    readonly_fields = ('date_joined', 'last_login')
     list_display = ('email', 'is_superuser', 'last_login', 'permitted_apps')
 
     def permitted_apps(self, obj):
         return ', '.join(f.name for f in obj.permitted_applications.all())
 
-    def has_add_permission(self, request):
-        return False
