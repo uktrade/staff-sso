6b2eca695e59b8d1f643ff433fbd1c99b5f35639
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2017-11-17 16:08:33 +0000

    Add admin filtering users by app

---
+++ sso/user/filter.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from sso.oauth2.models import Application
+
+
+class ApplicationFilter(admin.SimpleListFilter):
+
+    title = _('application')
+
+    parameter_name = 'application'
+
+    def lookups(self, request, model_admin):
+
+        return Application.objects.values_list('id', 'name') 
+
+    def queryset(self, request, queryset):
+
+        if self.value():
+            return queryset.filter(permitted_applications__id=self.value())
