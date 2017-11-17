512ead86074ac29f59269a6e5b09bc0da86a2acc
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2017-11-17 14:23:34 +0000

    Add a basic healthcheck url

---
+++ sso/tests/test_healthcheck.py
@@ -0,0 +1,36 @@
+import re
+import pytest
+from unittest.mock import MagicMock, patch
+
+import xml.etree.ElementTree as ET
+
+pytestmark = [
+    pytest.mark.django_db
+]
+
+
+class TestHealthCheck:
+    def test_check_view(self, client):
+
+        response = client.get('/check')
+
+        assert response.status_code == 200
+
+        xml = ET.fromstring(response.content)
+        status = xml[0].text
+        response_time = xml[1].text
+
+        assert status == 'OK'
+        assert re.match('^[\d\.]+$', response_time)
+
+    @patch('sso.healthcheck.views.User.objects.all', MagicMock(side_effect=Exception('something bad happened')))
+    def test_check_view_broken_database(self, client):
+
+        response = client.get('/check')
+
+        assert response.status_code == 200
+
+        xml = ET.fromstring(response.content)
+        status = xml[0].text
+
+        assert status == 'FAIL'
