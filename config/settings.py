63dfb1c4897f6ea87353a95943cafd2b9d736492
Stuart Axon <stu.axon@gmail.com>
2018-07-18 14:16:14 +0100

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ if os.path.exists(ENV_FILE):
     environ.Env.read_env(ENV_FILE)
 env = environ.Env(
     DEBUG=(bool, False),
-    RESTRICT_ADMIN_BY_IPS=(bool, False),
+    RESTRICT_ADMIN=(bool, False),
 )
 
 SECRET_KEY = env('SECRET_KEY')
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'sso.core.middleware.NeverCacheMiddleware',
-    # 'ip_restriction.IpWhitelister',
+    'admin_ip_restrictor.middleware.AdminIPRestrictorMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
