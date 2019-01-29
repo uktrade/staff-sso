54398a9dd6dbf4697a01a4dc1326d5942e171ba5
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-01-29 17:47:41 +0000

    Remove old report code

--- sso/user/admin_urls.py
+++ sso/user/admin_urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import url
 
 from .admin_views import (
-    AdminUserAliasAddImportView,
-    AdminUserMergeImportView,
     ShowUserPermissionsView,
     UserDataExportView,
-    UserPermissionExportView
 )
 
 urlpatterns = [
-    url(r'^user-import/$', AdminUserMergeImportView.as_view(), name='admin-user-merge-import'),
-    url(r'^user-alias-import/$', AdminUserAliasAddImportView.as_view(), name='admin-user-alias-import'),
     url(r'^user/export-list/$', UserDataExportView.as_view(), name='user-export-view'),
-    url(r'^user/export-permissions-list/$', UserPermissionExportView.as_view(), name='user-permission-export-view'),
     url(r'^user/show-permissions/(?P<user_id>\d+)/$', ShowUserPermissionsView.as_view(), name='show-permissions-view'),
 ]
--- sso/user/data_export.py
+++ sso/user/data_export.py
@@ -1,7 +1,5 @@
 from django.contrib.auth import get_user_model
 
-from sso.oauth2.models import Application
-
 
 class UserDataExport:
     def __iter__(self):
@@ -24,40 +22,3 @@ class UserDataExport:
             row.extend(user.emails.exclude(email=user.email).values_list('email', flat=True))
 
             yield row
-
-
-class UserPermissionExport:
-    """This will export a list of users: primary email, first name, last name, **all applications"""
-
-    def get_application_list(self):
-        applications = list(Application.objects.all().values_list('id', 'name'))
-        self._app_ids, self._app_names = zip(*applications)
-
-    def get_query(self):
-        return get_user_model().objects.all().prefetch_related('permitted_applications').order_by('email')
-
-    def get_header(self):
-        return ['email', 'first_name', 'last_name'] + list(self._app_names)
-
-    def get_user_application_columns(self, user):
-        cols = []
-
-        for app_id in self._app_ids:
-            if user.permitted_applications.filter(pk=app_id).exists():
-                cols.append('y')
-            else:
-                cols.append('')
-
-        return cols
-
-    def format_row(self, user):
-        return [user.email, user.first_name, user.last_name] + self.get_user_application_columns(user)
-
-    def __iter__(self):
-        self.get_application_list()
-        users = self.get_query()
-
-        yield self.get_header()
-
-        for user in users:
-            yield self.format_row(user)
