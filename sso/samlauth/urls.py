b8d1240cea9939c7690b85bf9e73d28452516a09
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-02-04 16:21:28 +0000

    Log authenticate with idps

--- sso/samlauth/urls.py
+++ sso/samlauth/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', views.login, name='saml2_login'),
-    url(r'^acs/$', djangosaml2_views.assertion_consumer_service, name='saml2_acs'),
+    #url(r'^acs/$', djangosaml2_views.assertion_consumer_service, name='saml2_acs'),
+    url(r'^acs/$', views.assertion_consumer_service, name='saml2_acs'),
     url(r'^logout/$', views.logout, name='saml2_logout'),
     url(r'^ls/post/$', djangosaml2_views.logout_service_post, name='saml2_ls_post'),
     url(r'^metadata/$', djangosaml2_views.metadata, name='saml2_metadata'),
