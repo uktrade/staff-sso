40591330e580350996778eace05d989d15ad24da
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-06-13 19:10:47 +0100

    Add O365 metadata for staging/UAT environment

--- config/settings.py
+++ config/settings.py
@@ -239,7 +239,8 @@ if ENV_NAME == 'staging':
         os.path.join(SAML_CONFIG_DIR, 'idp_metadata_okta.xml'),
         os.path.join(SAML_CONFIG_DIR, 'idp_metadata_ukef.xml'),
         os.path.join(SAML_CONFIG_DIR, 'idp_metadata_google.xml'),
-        os.path.join(SAML_CONFIG_DIR, 'idp_metadata.xml')
+        os.path.join(SAML_CONFIG_DIR, 'idp_metadata.xml'),
+        os.path.join(SAML_CONFIG_DIR, 'idp_metadata_core.xml'),
     ]
 
     SAML_CONFIG['entityid'] = 'https://sso.uat.staff.service.trade.gov.uk/sp'
