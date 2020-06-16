b99a97f7fcb6463eeb90953bd291eabb8fd1404d
Michal Charemza <michal@charemza.name>
2020-06-16 14:24:56 +0100

    (feat) Forbid requests to Activity Stream via public internet

--- sso/core/views.py
+++ sso/core/views.py
@@ -6,6 +6,16 @@ from django.views.decorators.csrf import csrf_exempt
 @require_GET
 @csrf_exempt
 def activity_stream(request):
+    def forbidden():
+        return JsonResponse(
+            data={},
+            status=403,
+        )
+
+    via_public_internet = 'x-forwarded-for' in request.headers
+    if via_public_internet:
+        return forbidden()
+
     return JsonResponse(
         data={},
         status=200,
--- sso/tests/test_activity_stream.py
+++ sso/tests/test_activity_stream.py
@@ -2,7 +2,14 @@ from django.urls import reverse
 import pytest
 
 
-def test(api_client):
+def test_via_public_internet_then_403(api_client):
+    response = api_client.get(reverse('api-v1:core:activity-stream'),
+        HTTP_X_FORWARDED_FOR='',
+    )
+    assert response.status_code == 403
+    assert response.json() == {}
+
+
+def test_via_internal_networking_then_200(api_client):
     response = api_client.get(reverse('api-v1:core:activity-stream'))
     assert response.status_code == 200
-    assert response.json() == {}
