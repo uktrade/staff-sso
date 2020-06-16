2c3b12fa17e4234d043206595fb653b45ed36016
Michal Charemza <michal@charemza.name>
2020-06-16 18:13:17 +0100

    (feat) Hawk authentication on Activity Stream endpoint

--- sso/core/views.py
+++ sso/core/views.py
@@ -1,6 +1,9 @@
+import hmac
 from django.http import JsonResponse
+from django.conf import settings
 from django.views.decorators.http import require_GET
 from django.views.decorators.csrf import csrf_exempt
+from hawkserver import authenticate_hawk_header
 
 
 @require_GET
@@ -12,8 +15,48 @@ def activity_stream(request):
             status=403,
         )
 
+
+    ############################################
+    ## Ensure not accessed via public networking
+
     via_public_internet = 'x-forwarded-for' in request.headers
     if via_public_internet:
+        return forbidden()
+
+
+    ###########################
+    ## Ensure signed with Hawk
+
+    def lookup_credentials(passed_id):
+        user = {
+            'id': settings.ACTIVITY_STREAM_HAWK_ID,
+            'key':  settings.ACTIVITY_STREAM_HAWK_SECRET,
+        }
+        return \
+            user if hmac.compare_digest(passed_id, user['id']) else \
+            None
+
+    def seen_nonce(nonce, id):
+        # No replay attack prevention since no shared cache between instances,
+        # but we're ok with that for now
+        return False
+
+    try:
+        auth_header = request.headers['authorization']
+    except KeyError:
+        return forbidden()
+
+    # This is brittle to not running in PaaS or not via private networking
+    host, port = request.META['HTTP_HOST'].split(':')
+
+    max_skew_seconds = 15
+    error_message, credentials = authenticate_hawk_header(
+        lookup_credentials, seen_nonce, max_skew_seconds,
+        request.headers['authorization'],
+        request.method, host, port, request.get_full_path(),
+        request.headers.get('content-type', ''), request.body,
+    )
+    if error_message is not None:
         return forbidden()
 
     return JsonResponse(
--- sso/tests/test_activity_stream.py
+++ sso/tests/test_activity_stream.py
@@ -1,15 +1,96 @@
+import mohawk
 from django.urls import reverse
 import pytest
 
 
 def test_via_public_internet_then_403(api_client):
-    response = api_client.get(reverse('api-v1:core:activity-stream'),
+    path = reverse('api-v1:core:activity-stream')
+    host = 'localhost:8080'
+    url = f'http://{host}{path}'
+    response = api_client.get(path,
+        content_type=None,
         HTTP_X_FORWARDED_FOR='',
+        HTTP_HOST='localhost:8080',
+        HTTP_AUTHORIZATION=hawk_auth_header('the-id', 'the-secret', url, 'GET', b'', ''),
     )
     assert response.status_code == 403
     assert response.json() == {}
 
 
-def test_via_internal_networking_then_200(api_client):
-    response = api_client.get(reverse('api-v1:core:activity-stream'))
+def test_no_auth_header_then_403(api_client):
+    path = reverse('api-v1:core:activity-stream')
+    host = 'localhost:8080'
+    url = f'http://{host}{path}'
+    response = api_client.get(path,
+        content_type=None,
+        HTTP_HOST='localhost:8080',
+    )
+    assert response.status_code == 403
+    assert response.json() == {}
+
+
+@pytest.mark.parametrize('get_hawk_params', [
+    (lambda url: ('not-the-id', 'the-secret', url, 'GET', b'', '')),
+    (lambda url: ('the-id', 'not-the-secret', url, 'GET', b'', '')),
+    (lambda url: ('the-id', 'the-secret', url + 'not', 'GET', b'', '')),
+    (lambda url: ('the-id', 'the-secret', url, 'POST', b'', '')),
+    (lambda url: ('the-id', 'the-secret', url, 'GET', b'not', '')),
+    (lambda url: ('the-id', 'the-secret', url, 'GET', b'', 'not')),
+])
+def test_bad_auth_header_then_403(api_client, get_hawk_params):
+    path = reverse('api-v1:core:activity-stream')
+    host = 'localhost:8080'
+    url = f'http://{host}{path}'
+    response = api_client.get(path,
+        content_type=None,
+        HTTP_HOST='localhost:8080',
+        HTTP_AUTHORIZATION=hawk_auth_header(*get_hawk_params(url)),
+    )
+    assert response.status_code == 403
+    assert response.json() == {}
+
+
+def test_via_internal_networking_with_content_type_then_200(api_client):
+    # The Activity Stream does not send a content-type header. However, we
+    # shouldn't be brittle WRT that
+    path = reverse('api-v1:core:activity-stream')
+    host = 'localhost:8080'
+    url = f'http://{host}{path}'
+    response = api_client.get(path,
+        content_type='text/plain',
+        HTTP_HOST='localhost:8080',
+        HTTP_AUTHORIZATION=hawk_auth_header('the-id', 'the-secret', url, 'GET', b'', 'text/plain'),
+    )
     assert response.status_code == 200
+
+
+def test_via_internal_networking_with_query_string_then_200(api_client):
+    path = reverse('api-v1:core:activity-stream') + '?some=param&another=param'
+    host = 'localhost:8080'
+    url = f'http://{host}{path}'
+    response = api_client.get(path,
+        content_type=None,
+        HTTP_HOST='localhost:8080',
+        HTTP_AUTHORIZATION=hawk_auth_header('the-id', 'the-secret', url, 'GET', b'', ''),
+    )
+    assert response.status_code == 200
+
+
+def test_via_internal_networking_no_content_type_then_200(api_client):
+    path = reverse('api-v1:core:activity-stream')
+    host = 'localhost:8080'
+    url = f'http://{host}{path}'
+    response = api_client.get(path,
+        content_type=None,
+        HTTP_HOST='localhost:8080',
+        HTTP_AUTHORIZATION=hawk_auth_header('the-id', 'the-secret', url, 'GET', b'', ''),
+    )
+    assert response.status_code == 200
+
+
+def hawk_auth_header(key_id, secret_key, url, method, content, content_type):
+    return mohawk.Sender({
+        'id': key_id,
+        'key': secret_key,
+        'algorithm': 'sha256',
+    }, url, method, content=content, content_type=content_type).request_header
