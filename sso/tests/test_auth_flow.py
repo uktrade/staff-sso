86cf1cc4cdffeea53deb6eef68dd8d8a6fc613bd
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-07-15 18:56:12 +0100

    Add is_active check to saml auth backend

--- sso/tests/test_auth_flow.py
+++ sso/tests/test_auth_flow.py
@@ -226,6 +226,31 @@ class TestSAMLLogin:
         assert 'code' in authorize_qs
 
     @freeze_time('2017-06-22 15:50:00.000000+00:00')
+    def test_saml_login_with_inactive_user_fails(self, client, mocker):
+        """
+        If `user.is_active` is False, the user should not be able to authenticate.
+        """
+
+        UserFactory(email='user1@example.com', is_active=False)
+
+        data = {
+            'SAMLResponse': [base64.b64encode(get_saml_response(action='login'))],
+            'RelayState': ''
+        }
+
+        assert User.objects.count() == 1
+
+        MockOutstandingQueriesCache = mocker.patch('sso.samlauth.views.OutstandingQueriesCache')
+        MockOutstandingQueriesCache().outstanding_queries.return_value = {'id-WmZMklyFygoDg96gy': 'test'}
+
+        MockCryptoBackendXmlSec1 = mocker.patch('saml2.sigver.CryptoBackendXmlSec1', spec=True)
+        MockCryptoBackendXmlSec1().validate_signature.return_value = True
+
+        response = client.post(SAML_ACS_URL, data)
+
+        assert response.status_code == 403
+
+    @freeze_time('2017-06-22 15:50:00.000000+00:00')
     def test_x_application_saml_log_message(self, client, mocker):
         """
         Test that an x-application log message is created
