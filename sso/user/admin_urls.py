c3e412b019397d4a652065ecb59f3f856f697d64
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-10-06 13:09:03 +0100

    The csv export link has been removed from the django admin
    A future PR will remove the csv code

--- sso/user/admin_urls.py
+++ sso/user/admin_urls.py
@@ -7,7 +7,7 @@ from .admin_views import (
 )
 
 urlpatterns = [
-    url(r'^user/export-list/$', UserDataExportView.as_view(), name='user-export-view'),
-    url(r'^user/export-email-list/$', EmailLastLoginExportView.as_view(), name='email-export-view'),
+    # url(r'^user/export-list/$', UserDataExportView.as_view(), name='user-export-view'),
+    # url(r'^user/export-email-list/$', EmailLastLoginExportView.as_view(), name='email-export-view'),
     url(r'^user/show-permissions/(?P<user_id>\d+)/$', ShowUserPermissionsView.as_view(), name='show-permissions-view'),
 ]
