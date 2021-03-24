24380d2669b5259a9554775a0df0e082f5884be6
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2021-03-24 11:41:23 +0000

    Remove new saml idp paths from metadata
    The new paths with trailing /'s cause ELK to die with:
    
    "type":"exception","reason":"Found multiple locations for binding [urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect] in descriptor [null]
    
    Since ELK is the only app that dynamically pulls metadata, we're temporarily rolling back to legacy idp urls (without slashes).

--- config/settings.py
+++ config/settings.py
@@ -418,12 +418,13 @@ SAML_IDP_CONFIG = {
             "endpoints": {
                 "single_sign_on_service": [
                     # new post binding path
-                    (os.path.join(BASE_URL, "idp/sso/post/"), saml2.BINDING_HTTP_POST),
-                    (
-                        os.path.join(BASE_URL, "idp/sso/redirect/"),
-                        saml2.BINDING_HTTP_REDIRECT,
-                    ),
+                    # (os.path.join(BASE_URL, "idp/sso/post/"), saml2.BINDING_HTTP_POST),
+                    # (
+                    #     os.path.join(BASE_URL, "idp/sso/redirect/"),
+                    #     saml2.BINDING_HTTP_REDIRECT,
+                    # ),
                     # legacy paths - can be removed when all SPs have updated IdP metadata
+                    # with new paths
                     (os.path.join(BASE_URL, "idp/sso/post"), saml2.BINDING_HTTP_POST),
                     (
                         os.path.join(BASE_URL, "idp/sso/redirect"),
